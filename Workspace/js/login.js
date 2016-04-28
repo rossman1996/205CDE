57b41e641eb0aca558ff4b593dcbe55506d0a774
millerr8 <millerr8@uni.coventry.ac.uk>
2016-04-28 19:39:04 +0000

    I waste of a day

--- Workspace/js/login.js
+++ Workspace/js/login.js
@@ -1,1 +1,11 @@
-// JavaScrip
+// JavaScript
+    //get form references
+    
+    function functionz(){
+        if (document.getElementById("username").value == "student"){
+            document.getElementById('change').innerHTML = document.getElementById("username").value;
+            window.location= "http://www.bacon.com";
+        };
+
+    }
+    
